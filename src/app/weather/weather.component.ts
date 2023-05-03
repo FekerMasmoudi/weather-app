3c46eef03c588a8612e101162c4245a6f309f341
FekerMasmoudi <feker.masmoudi@gmail.com>
2023-05-03 14:55:26 +0100

    first commit

---
+++ src/app/weather/weather.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-weather',
+  templateUrl: './weather.component.html',
+  styleUrls: ['./weather.component.css']
+})
+export class WeatherComponent {
+
+}
